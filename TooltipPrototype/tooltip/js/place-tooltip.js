bc3c286dfa4f3c3ada30cb710c4812f7a8cb5379
Matteo <palamatteocp2004@gmail.com>
2021-03-28 00:32:09 +0100

    Fake_1.1
    HTML and CSS fixed
    Start to implement Tooltip

---
+++ TooltipPrototype/tooltip/js/place-tooltip.js
@@ -0,0 +1,64 @@
+/********* PARAMETERS *********/
+const pointerSelector = ".tooltipBox";
+
+/********* REFERENCES *********/
+let pointer;
+
+/********* STATES *********/
+let cursorSize;
+let windowSize;
+let cursorOffset = {x: 10, y: 10};
+
+window.addEventListener("load", () => {
+    pointer = document.querySelector(pointerSelector);
+    cursorSize = GetSize(pointer);
+    bodySize = GetSize(document.body);
+    windowSize = {x: window.innerWidth, y: window.innerHeight}
+});
+
+window.addEventListener("mousemove", e => {
+    let event = e ? e : window.event;
+    SetPointerPosition(GetCursorPos(event));
+});
+
+function GetCursorPos(e) {
+    let cursorPos = {x: e.pageX, y: e.pageY};
+    let newPos = cursorPos;
+
+    if (cursorPos.x < windowSize.x/2)
+        newPos.x += cursorOffset.x;
+    else
+        newPos.x -= cursorSize.x + cursorOffset.x;
+
+    if (cursorPos.y < windowSize.y/2)
+        newPos.y += cursorOffset.y;
+    else
+        newPos.y -= cursorSize.y + cursorOffset.y;
+
+    return newPos;
+}
+
+function SetPointerPosition(cursorPos) {
+    pointer.style.left = cursorPos.x + "px";
+    pointer.style.top = cursorPos.y + "px";
+}
+
+function GetSize(element) {
+    let elementRect = element.getBoundingClientRect();
+    return {x: elementRect.right - elementRect.left, y: elementRect.bottom - elementRect.top}
+}
+
+/*
+function CheckOutOfScreen(item) {
+    let bounding = item.getBoundingClientRect();
+
+	let out = {};
+	out.top = bounding.top < 0;
+	out.left = bounding.left < 0;
+	out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+	out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+	out.any = out.top || out.left || out.bottom || out.right;
+	out.all = out.top && out.left && out.bottom && out.right;
+
+	return out;
+}*/
