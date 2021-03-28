6517722bf57afc3c7e42822dea94bb4e300a7556
Matteo <palamatteocp2004@gmail.com>
2021-03-28 01:32:42 +0100

    Fake_1.3
    Mobile Freindly 1.0

--- Fake/tooltip/js/place-tooltip.js
+++ Fake/tooltip/js/place-tooltip.js
@@ -6,15 +6,14 @@ let pointer;
 
 /********* STATES *********/
 let cursorSize;
-let windowSize;
+let clientSize;
 let cursorOffset = {x: 10, y: 10};
 
 window.addEventListener("load", () => {
     pointer = document.querySelector(pointerSelector);
     cursorSize = GetSize(pointer);
     bodySize = GetSize(document.body);
-    windowSize = {x: document.documentElement.clientWidth, y: document.documentElement.clientHeight};
-    console.log(windowSize);
+    clientSize = {x: document.documentElement.clientWidth, y: document.documentElement.clientHeight};
 });
 
 window.addEventListener("mousemove", e => {
@@ -26,12 +25,12 @@ function GetCursorPos(e) {
     let cursorPos = {x: e.clientX, y: e.clientY};
     let newPos ={x: e.pageX, y: e.pageY};
 
-    if (cursorPos.x < windowSize.x/2)
+    if (cursorPos.x < clientSize.x/2)
         newPos.x += cursorOffset.x;
     else
         newPos.x -= cursorSize.x + cursorOffset.x;
 
-    if (cursorPos.y < windowSize.y/2)
+    if (cursorPos.y < clientSize.y/2)
         newPos.y += cursorOffset.y;
     else
         newPos.y -= cursorSize.y + cursorOffset.y;
@@ -48,18 +47,3 @@ function GetSize(element) {
     let elementRect = element.getBoundingClientRect();
     return {x: elementRect.right - elementRect.left, y: elementRect.bottom - elementRect.top}
 }
-
-/*
-function CheckOutOfScreen(item) {
-    let bounding = item.getBoundingClientRect();
-
-	let out = {};
-	out.top = bounding.top < 0;
-	out.left = bounding.left < 0;
-	out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
-	out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
-	out.any = out.top || out.left || out.bottom || out.right;
-	out.all = out.top && out.left && out.bottom && out.right;
-
-	return out;
-}*/
